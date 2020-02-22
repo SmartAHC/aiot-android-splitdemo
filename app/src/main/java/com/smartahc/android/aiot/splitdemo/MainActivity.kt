d9f6a03e60c43445f900ccb23afce47bb4ec6c4a
yuan <labelnet@smartahc.com>
2020-02-22 08:35:22 +0800

    init project

---
+++ app/src/main/java/com/smartahc/android/aiot/splitdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.smartahc.android.aiot.splitdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
