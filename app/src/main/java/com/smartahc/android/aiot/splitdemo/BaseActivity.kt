348d832cb68301743d1a4406f560a81e0cf88ea9
yuan <labelnet@smartahc.com>
2020-02-28 20:40:00 +0800

    添加页面跳转和信息广播

---
+++ app/src/main/java/com/smartahc/android/aiot/splitdemo/BaseActivity.kt
@@ -0,0 +1,117 @@
+package com.smartahc.android.simple
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.util.concurrent.atomic.AtomicBoolean
+
+
+/**
+ * 权限申请
+ */
+abstract class BaseActivity : AppCompatActivity() {
+
+    companion object{
+        const val REQUEST_PERMISSIONS = 10001
+    }
+
+    /**
+     * 是否有权限
+     */
+    protected val hasPermission: AtomicBoolean = AtomicBoolean(false)
+
+
+    /**
+     * 检测权限
+     */
+    protected fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permissions = this.getPermissions()
+            if (permissions.isEmpty()) {
+                return
+            }
+            var grantedCount = 0
+
+            for (permission in permissions) {
+                if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                    break
+                }
+                grantedCount++
+            }
+            if (grantedCount == permissions.size) {
+                // 全部通过
+                hasPermission.set(true)
+            } else {
+                hasPermission.set(false)
+                requestPermissions(permissions, REQUEST_PERMISSIONS)
+            }
+        }
+    }
+
+    /**
+     * 权限判断
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSIONS) {
+            var permissionCount = 0
+            // 直到有权限为止
+            for ((index, value) in grantResults.withIndex()) {
+                if (value != PackageManager.PERMISSION_GRANTED) {
+                    val isTips = ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        permissions[index]
+                    )
+                    if (isTips) {
+                        checkPermission()
+                    } else {
+                        intoPermissionSettingPage()
+                    }
+                    break
+                }
+                permissionCount += 1
+            }
+            if (permissionCount == permissions.size) {
+                hasPermission.set(true)
+            }
+        }
+    }
+
+
+    /**
+     * 进入权限页面
+     */
+    protected fun intoPermissionSettingPage(){
+        this.showToastMsg("无权限，请全部授权")
+        val intent = Intent()
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
+        intent.data = Uri.fromParts("package", packageName, null)
+        this.startActivity(intent)
+        finish()
+    }
+
+
+    /**
+     * 获取权限列表
+     */
+    protected abstract fun getPermissions(): Array<String>
+
+    /**
+     * 显示信息
+     */
+    protected fun showToastMsg(msg: String) {
+        this.runOnUiThread {
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
--- app/src/main/java/com/smartahc/android/aiot/splitdemo/MainActivity.kt
+++ app/src/main/java/com/smartahc/android/aiot/splitdemo/MainActivity.kt
@@ -1,12 +1,108 @@
 package com.smartahc.android.aiot.splitdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import com.smartahc.android.simple.BaseActivity
+import com.smartahc.android.splitcore.Common
+import com.smartahc.android.splitcore.PageParams
+import com.smartahc.android.splitcore.SmartUser
+import com.smartahc.android.splitcore.SplitCore
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+
+    /**
+     * 申请权限
+     */
+    override fun getPermissions(): Array<String> {
+        return arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 申请权限
+        checkPermission()
+        // Step 1：注意：请联系商务获取账户密码！！
+        SplitCore.onSplitCreate(this, SmartUser("15626093012", "1234567"))
+        // 测试初始化
+        btnInit.setOnClickListener {
+            SplitCore.onSplitResume()
+        }
+        // 功能：进入菜单
+        btnMenu.setOnClickListener {
+            SplitCore.onSkipMenu()
+        }
+        // 功能：进入戴标界面
+        btnWearTag.setOnClickListener {
+            // 注意：请登录网址端获取配置！！
+            val params =
+                "{\"id\":1,\"func\":1,\"hardware_selection\":[\"耳标钳\",\"伸缩阅读器\"],\"categoryfunc\":147}"
+            SplitCore.onSkipPage(params)
+        }
+        // 功能：进入戴标界面
+        btnWearTagParams.setOnClickListener {
+            // 注意：请登录网址端获取配置！！
+            SplitCore.onSkipPage(
+                PageParams(
+                    1, 1, 1, listOf(
+                        "耳标钳", "伸缩阅读器"
+                    )
+                )
+            )
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Step 2
+        SplitCore.onSplitResume()
+        registryBroadcast()
+    }
+
+    override fun onDestroy() {
+        unRegistryBroadcast()
+        super.onDestroy()
+    }
+
+    /**
+     * 注册广播
+     */
+    private fun registryBroadcast() {
+        registerReceiver(broadcast, IntentFilter(Common.SERVICE_BROADCAST_ACTION_FILTER))
+    }
+
+    /**
+     * 解注册
+     */
+    private fun unRegistryBroadcast() {
+        unregisterReceiver(broadcast)
+    }
+
+    /**
+     * 接收
+     */
+    private val broadcast = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.action) {
+                Common.SERVICE_BROADCAST_ACTION_FILTER -> {
+                    // 获取数据
+                    val earTag = intent.getStringExtra(Common.SERVICE_BROADCAST_KEY_EARTAG)
+                    runOnUiThread {
+                        tvEartag.text = earTag
+                    }
+                }
+            }
+        }
+    }
+
 }
